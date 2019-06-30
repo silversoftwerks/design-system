a32ca7ee83c3d386087b8d15a949fa11bc11494a
Silver Softwerks <engineering@silversoftwerks.com>
2019-06-30 17:02:55 -0600

    Remove forward refs

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Box from "./Box";
 
-const Page = React.forwardRef(
-  ({ children, width = "100%", height = "100%", ...rest }, ref) => (
-    <Box tag="section" ref={ref} {...rest} flexDirection="column">
-      {children}
-    </Box>
-  )
+const Page = ({
+  children,
+  width = "100%",
+  height = "100%",
+  measureRef,
+  ...rest
+}) => (
+  <Box tag="section" measureRef={measureRef} {...rest} flexDirection="column">
+    {children}
+  </Box>
 );
-Page.displayName = "Page";
 
 export default Page;
